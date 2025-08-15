5381f4ebddfa4d223806d32ed641d12edd6add6c
mds126 <zack126123126@gmail.com>
2025-08-15 15:20:13 +0200

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -9,7 +9,7 @@ const ASSETS = [
   "/icons/icon-192.png",
   "/icons/icon-512.png"
 ];
-
+navigator.serviceWorker.register('./sw.js');
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll(ASSETS))
